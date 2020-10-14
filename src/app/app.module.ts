448768d1e0e4b579a73357c693ba82665cd2236e
Dave Gedert <DaveGedert@QuickenLoans.com>
2020-10-14 09:15:18 -0400

    Forgot the HTTP MODULE! WORKS NOW!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ResultComponent } from './components/result/result.component';
 import { ListComponent } from './components/list/list.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { ListComponent } from './components/list/list.component';
     ListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
